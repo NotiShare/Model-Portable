c50d0ffd6f74ef03a14fd1908b50c3acdbd780b7
FezZHD <fezzdev@gmail.com>
2017-12-14 05:07:28 +0300

    adding getting ws address from resource file

---
+++ NotiShareModel/CrossHelper/CrossResourceHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotiShareModel.CrossHelper
+{
+    public static class CrossResourceHelper
+    {
+
+        public static string GetWsAdress()
+        {
+            return UrlResource.WebSocketUrl;
+        }
+    }
+}
